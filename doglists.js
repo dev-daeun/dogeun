f9ef7c47db8a46c504945787ae29f245944c331a
Yeonsu <gin7566@gmail.com>
2017-05-31 09:12:59 +0900

    doglists.js

--- doglists.js
+++ doglists.js
@@ -29,43 +29,37 @@ const arrUpload = upload.fields([{ name: 'pet', maxCount: 3 }, { name: 'lineage'
 
 router.post('/', arrUpload, addParcels);
 router.put('/', arrUpload, changeParcels);
-router.delete('/', removeParcels);
+router.delete('/:parcel_id', removeParcels);
 
 function showReq(req, res, next) {
     console.log(req.headers);
     next();
 }
 
-async function removeParcels(req, res, next) {
 
-    let remove_id = req.body.parcel_id;
-
-    try {
-        let ret = await Parcels.deleteParcels(remove_id);
-        res.send({ message: 'save' });
-    } catch (err) {
-        console.log('err message : ', err);
-        res.send({ message: 'fail' });
-    }
-};
-
-
-
-async function deleteInS3(itemKey, path) {
+async function deleteInS3(itemKey) {
     return new Promise((resolve, reject) => {
-
+       
         const params = {
             Bucket: 'yeonsudogndogn',
-            Key: itemKey,
-            ACL: 'public-read',
-            Body: fs.createReadStream(path)
+            //Key : itemKey
+            Delete: {
+                Objects: [
+                    { 
+                        Key: itemKey 
+                    }
+                ]
+            }
+
         }
 
-        s3.deleteObjects(params, function (err, data) {
+        s3.deleteObjects(params, (err, data) => {
             if (err) {
-                return res.send({ "error": err });
+                reject(err);
+            } else {
+                console.log(data);
+                resolve(data);
             }
-            res.send({ data });
         });
     })
 };
@@ -96,6 +90,56 @@ async function uploadToS3(itemKey, path) {
     })
 }
 
+
+// 분양글 삭제, s3 이미지까지 삭제
+async function removeParcels(req, res, next) {
+
+    let remove_id = req.params.parcel_id;
+
+    try {
+        // s3 이미지 지우기
+        let whatIs = 'pet';
+        let pets = await Parcels.searchImage(remove_id,whatIs);
+        var petKey = [];
+
+        for (let petItem of pets) {
+            petKey = petItem.image.split('/');
+            await deleteInS3(petKey[3]);
+        }
+        
+        whatIs = 'parent';
+        let parents = await Parcels.searchImage(remove_id,whatIs);
+        var parentKey = [];
+
+        for(let parentItem of parents){
+            parentKey = parentItem.image.split('/');
+            await deleteInS3(parentKey[3]);
+        }
+
+        whatIs = 'parcel';
+        let parcels = await Parcels.searchImage(remove_id,whatIs);
+        var thumbnailKey = [];
+        
+        thumbnailKey = parcels[0].pet_thumbnail.split('/');
+        await deleteInS3(thumbnailKey[4]);
+
+        var lineageKey = [];
+
+        lineageKey = parcels[0].lineage.split('/');
+        await deleteInS3(lineageKey[3]);
+        
+        let result = Parcels.deleteParcles(remove_id);
+       
+        res.status(200).send({ message: 'save' });
+    } catch (err) {
+        console.log('err message : ', err);
+        res.status(500).send({ message: 'fail' });
+    }
+};
+
+
+
+
 async function changeParcels(req, res, next) {
     let change_id = req.body.parcel_id;
 
@@ -106,12 +150,15 @@ async function changeParcels(req, res, next) {
 
     let parent_image_records = [];
 
+    console.log(req.files['parent']);
     for (let i in req.files['parent']) {
         parent_image_records.push({
-            'image': req.files['parent'][i].location,
-            'image_id': req.body.image_id
+            'image': req.files['parent'][i].location
+            
         });
     }
+
+
 
     console.log(parent_image_records);
     //pet_images 테이블에 들어갈 record 배열 
@@ -139,26 +186,39 @@ async function changeParcels(req, res, next) {
 
 async function addParcels(req, res, next) {
 
+    //error 처리
+    if (!req.body.user_id || !req.body.spiece || !req.body.gender || !req.body.age || !req.body.region1
+        || !req.body.region2 || !req.body.price || !req.body.size || !req.body.introduction
+        || !req.body.condition || !req.body.title) {
+
+        res.status(400).send({ message: 'fail' });
+        return;
+    }
+    
     //파일 제외하고 body부분 record
     let parcel_records = req.body;
 
     //parcel 테이블에 들어갈 파일 record 추가
+    if(!req.files['lineage']){
+        parcel_records.lineage = null;
+    }else{
     parcel_records.lineage = req.files['lineage'][0].location;
+    }
 
-
-    // parent_image_records 테이블에 들어갈 record
+    // parent_image_records 테이블에 들어갈 record 추가
     let parent_image_records = [];
 
     if (req.files['parent'] instanceof Array) {
 
-         // parent 이미지 파일 개수만큼 record 추가 , key값과 함께 배열에 push
+        // parent 이미지 파일 개수만큼 record 추가 , key값과 함께 배열에 push
         for (let i in req.files['parent']) {
             parent_image_records.push({ 'image': req.files['parent'][i].location });
         }
-
-    } else {
+    } else if(req.files['parent'])
+    {
         parent_image_records.push({ 'image': req.files['parent'][0].location });
     }
+
 
     //pet_images 테이블에 들어갈 record 배열 
     let image_records = [];
@@ -174,8 +234,8 @@ async function addParcels(req, res, next) {
     } else {
         image_records.push({ 'image': req.files['pet'][0].location });
     }
-
-
+    
+  
     // 썸네일 만드는 부분 
     let thumnail_fileName = 'thumnbnail_' + req.files['pet'][0].key;
 
@@ -197,15 +257,43 @@ async function addParcels(req, res, next) {
     // record 넘기고 클라이언트에 응답
 
     try {
+     
         let ret = await Parcels.postParcels(parcel_records, parent_image_records, image_records);
         res.send({ message: 'save' });
     }
     catch (err) {
         console.log('error message : ', err);
-        res.send({ message: 'fail' });
+        res.status(500).send({ message: 'fail' });
 
     }
 };
 
 
 module.exports = router;
+
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
--- parcels.js
+++ parcels.js
@@ -6,37 +6,71 @@ var pool = require('./db_pool');
 class Parcels {
 }
 
+Parcels.searchImage = async function (id,kinds) {
+    let connection;
+    let result;
+    try {
+        connection = await pool.getConnection();
+        if(kinds == 'pet'){
+        let query1 = 'SELECT parcel_id, image, image_id FROM pet_images WHERE parcel_id =?';
+        result = await connection.query(query1, id);
+    }
+    else if(kinds == 'parent'){
+        let query2 = 'SELECT parcel_id, image, image_id FROM parent_pet_images WHERE parcel_id = ?';
+        result = await connection.query(query2,id);
+    }
+    else if(kinds == 'parcel'){
+        let query3 = 'SELECT pet_thumbnail,lineage FROM parcel WHERE parcel_id = ?';
+        result = await connection.query(query3,id);
+    }
+   
 
-Parcels.postParcels = async function (record1, record2, record3) {
+        await connection.commit();
+        
+        return result;
+
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        pool.releaseConnection(connection);
+    }
+
+
+}
+
+
+Parcels.postParcels = async function (parcel_record, parent_record, pet_record) {
     let connection;
     try {
         connection = await pool.getConnection();
 
         await connection.beginTransaction();
 
-        let query1 = 'insert into parcel set ? ';
-        let parcel_output = await connection.query(query1, record1); //분양글 저장 -> 분양글 id가 parcel_id에 저장
+        let query1 = 'INSERT INTO parcel SET ? ';
+        let parcel_output = await connection.query(query1, parcel_record); //분양글 저장 -> 분양글 id가 parcel_id에 저장
         let parcel_id_out = parcel_output.insertId;
 
-        for (let i in record2) {
-            record2[i].parcel_id = parcel_id_out;
+  
+       
+        for (let parent of parent_record) {
+            parent.parcel_id = parcel_id_out;
+            let query2 = 'INSERT INTO parent_pet_images SET ? ';
+            await connection.query(query2, parent);
+            
         }
 
-        for (let i in record3) {
-            record3[i].parcel_id = parcel_id_out;
+        for (let pet of pet_record) {
+            pet.parcel_id = parcel_id_out;
+            let query3 = 'INSERT INTO pet_images SET ? ';
+            await connection.query(query3, pet);
         }
-
-        let query2 = 'insert into parent_pet_images set ? ';
-        await connection.query(query2, record2);
-
-        let query3 = 'INSERT INTO pet_images set ? ';
-        let complete = await connection.query(query3, record3);
 
 
         //commit
         await connection.commit();
 
-        return complete;
+        return parcel_output;
 
 
     } catch (err) {
@@ -54,7 +88,7 @@ Parcels.postParcels = async function (record1, record2, record3) {
 
 };
 
-Parcels.updateParcels = async function (id, record1, record2, record3) {
+Parcels.updateParcels = async function (id, parcel_record, parent_record, pet_record) {
     let connection;
     try {
         connection = await pool.getConnection();
@@ -62,14 +96,14 @@ Parcels.updateParcels = async function (id, record1, record2, record3) {
         await connection.beginTransaction();
 
         let query1 = 'UPDATE parcel SET ? WHERE parcel_id = ? ';
-        let parcel_output = await connection.query(query1, record1, id);
+        let parcel_output = await connection.query(query1, parcel_record, id);
         console.log(id);
 
         // let query2 = 'UPDATE parent_pet_images SET ? WHERE parcel_id = ?';
-        // let pa_output = await connection.query(query2, record2, id);
+        // let pa_output = await connection.query(query2, parent_record, id);
 
         // let query3 = 'UPDATE pet_images SET ? WHERE parcel_id = ? ';
-        // let complete = await conncetion.query(query3, record3, id);
+        // let complete = await conncetion.query(query3, pet_record, id);
 
         await conncetion.commit();
 
@@ -94,7 +128,7 @@ Parcels.deleteParcles = async function (id) {
     try {
         connection = await pool.getConnection();
 
-        let query = 'DELETE FROM parcels WHERE parcel = ?';
+        let query = 'DELETE FROM parcel WHERE parcel_id = ?';
         let result = await connection.query(query, id);
 
         return result;
