8add6dab870ceb475b58f493ed230d4f1c397f3c
kde6260 <kde6260@gmail.com>
2017-06-09 13:43:43 +0900

    chat methods updated

--- dogeun/model/chats.js
+++ dogeun/model/chats.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const user = require('../config/ORM');
+const User = require('../config/ORM');
+const moment = require('moment');
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error);
@@ -25,17 +26,102 @@ const RoomSchema = new Schema({
     created_time: String,
     chatters: [Number],  //채팅방 참여자 user_id
     remained_chatters: [Number], //채팅방에 남아있는 참여자 user_id 
-    messages: [MessageSchema] //메세지 담는 배열
+    messages: Array //메세지 담는 배열
 });
 const Message = mongoose.model('Message', MessageSchema);
-const Room = mongoose.model('Room', RoomSchema);
+const Room = mongoose.model('Room', RoomSchema); //스키마 모델
 class Chats{}
 
-Chats.getUnread = async function(room_id){
-    //TODO: is_read가 false인 메세지들의 갯수를 리턴
-
+Chats.getUnreadCount = async function(room_id){
+    try {
+        //is_read가 false인 메세지들의 갯수를 리턴
+        let count = await Message.find({ room_id: room_id, is_read: false }).count(); 
+        return count;
+    }
+    catch(err){
+        console.log(err);
+        throw err;
+    }
 };
 
+Chats.getUnread = async function(room_id){
+    try {
+            //is_read가 false인 메세지들을 리턴
+        let messages = await Message.find({ room_id: room_id, is_read: false }); 
+        return messages;
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+Chats.setRead = async function(room_id){
+    try {
+        let result = await Message.update({ room_id: room_id }, { is_read: true });
+        return result;
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+Chats.creatRoom = function(creator_id, participant_id){
+    try {
+          let new_room  = new Room({ //Room 모델로 채팅방 객체 생성
+            created_time: moment(new Date()).format('YY-MM-DD h:mm:ss a'),
+            chatters: [creator_id, participant_id],
+            remained_chatters: [creator_id, participant_id],
+            messages: []
+          });
+          let result = new_room.save(); //save()는 생성된 객체 그대로 리턴
+          return result;
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+Chats.saveMessage = async function(s_id, r_id, con, room_id){
+    try {
+        let sender, s_name, receiver, r_name;
+        sender = await User.findOne({
+            attributes: ['username'],
+            where: { user_id: s_id }
+        }); //보낸 사람 이름 user 테이블에서 가져오기
+        s_name = sender.dataValues.username;
+        receiver = await User.findOne({
+            attributes: ['username'],
+            where: { user_id: r_id }
+        }); //받은 사람 이름 user 테이블에서 가져오기
+        r_name = receiver.dataValues.username;
+        let new_msg  = new Message({ //Message 모델로 메세지 도큐먼트 생성
+            sender_id: s_id,
+            sender_name: s_name,
+            receiver_id: r_id,
+            receiver_name: r_name,
+            sent_time: moment(new Date()).format('YY-MM-DD h:mm:ss a'),
+            content: con,
+            is_read: false,
+            room_id: ObjectId(room_id)
+        });
+
+        let insertNew = await new_msg.save(); //Message 컬렉션에 message 추가
+        await Room.update( //Room 컬렉션에 message 추가
+            { _id: room_id },
+            { $push: { messages: insertNew }}
+        );
+        return insertNew;
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+
+
+};
 Chats.getRooms = async function(id){
     try {
         let rooms = await Room.find(  //채팅목록 조회
@@ -46,7 +132,7 @@ Chats.getRooms = async function(id){
         let images = []; //메세지를 보낸 sender들의 프로필 썸네일 url을 담을 배열
         for(let i = 0; i< rooms.length; i++){
             let recent = rooms[i].messages.length-1;
-            let profile = await user.findOne({
+            let profile = await User.findOne({
                 attributes: ['profile_thumbnail'],
                 where: { user_id: rooms[i].messages[recent].sender_id }}); 
                 //채팅방 별 가장 최근에 도착한 메세지를 보낸 사람의 id로 보낸 사람 프로필 썸네일 가져오기
--- dogeun/routes/chats.js
+++ dogeun/routes/chats.js
@@ -42,4 +42,5 @@ router.put('/', async (req, res) => {
     }
 
 });
+
 module.exports = router;
