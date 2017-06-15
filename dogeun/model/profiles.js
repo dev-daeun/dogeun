78ead97d5418bb5a3a4a250021b06060c1161031
kde6260 <kde6260@gmail.com>
2017-06-15 22:38:14 +0900

    프로필 조회 시 내가 쓴 글 목록 추가

--- dogeun/model/profiles.js
+++ dogeun/model/profiles.js
@@ -51,6 +51,29 @@ Profile.deleteFromS3 = function(key){
     });
 };
 
+
+Profile.readProfile = async function(id){
+    let connection;
+    let data = {};
+    try{
+        connection = await pool.getConnection();
+        let query = 'select profile_image, username, gender, lifestyle, region, other_pets, family_size, profile_thumbnail from users where user_id = ? ';
+        let user = await connection.query(query, id);
+        return user[0];
+    //    let keys = Object.keys(user[0]);
+    //    for(let item of keys ){
+    //        console.log(user[0][item]);
+    //        data[item] = user[0][item];
+    //    }
+    //     return data;
+    }catch(err){
+        console.log(err);
+        throw err;
+    }finally{
+        pool.releaseConnection(connection);
+    }
+;}
+
 Profile.saveProfile = async function(req){
     try {
             var connection = await pool.getConnection();
--- dogeun/routes/profiles.js
+++ dogeun/routes/profiles.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Profile = require('../model/profiles');
+const DogList = require('../model/doglists');
 const AWS = require('../config/AWS');
 AWS.loadAccess();
 const upload = AWS.getUpload();
@@ -20,8 +21,10 @@ router.get('/:user_id', async function(req,res){
         if(!userId){
             res.status(400).send({message: 'no user error'});
         }else{
-            let ret = await Profile.readProfile(userId);
-            res.status(200).send(ret);
+            let profile = await Profile.readProfile(userId);
+            let parcelList = await DogList.getMyLists(userId);
+            profile.mylist = parcelList;
+            res.status(200).send(profile);
         }
         
     }catch(err){
